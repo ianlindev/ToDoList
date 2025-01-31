c6b6aa3c811c0f56a56b4d286396b80c5962bb5e
Ian <ian0731@gmail.com>
2025-01-31 16:26:18 +0800

    feat(logging): 新增 Serilog 設定及日誌記錄

--- Program.cs
+++ Program.cs
@@ -1,32 +1,63 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Filters;
 using ToDoList.Models.EFModel;
 
-var builder = WebApplication.CreateBuilder(args);
+//取得組態設定檔
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
-// Add services to the container.
+//Log初始化 (捕捉host啟動的錯誤)
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()//讓log訊息多顯示一點
+    .ReadFrom.Configuration(configuration)// 從設定檔中讀取
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// 註冊 DbContext
-builder.Services.AddDbContext<ToDoListContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Serilog 兩階段初始化
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)// 從設定檔中讀取
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+    );
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // 註冊 DbContext
+    builder.Services.AddDbContext<ToDoListContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
